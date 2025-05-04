cbbd5c3dc2655549bf411f9cfe3f6620b578eaf6
Ваше ім'я < email>
2025-05-04 19:21:42 +0300

    bag fix

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,19 @@
-const generateContacts = async (number) => {};
+import fs from 'fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
 
-generateContacts(5);
+const generateContacts = async (number) => {
+  const newContact = await readContacts();
+
+  for (let i = 0; i < number; i++) {
+    newContact.push(createFakeContact());
+  }
+  await fs.writeFile(PATH_DB, JSON.stringify(newContact, null, 1), 'utf-8');
+  try {
+  } catch (error) {
+    throw error;
+  }
+};
+
+generateContacts(1);
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
-
-export const readContacts = async () => {};
+import fs from 'fs/promises';
+export const readContacts = async () => {
+  try {
+    const contacts = await fs.readFile(PATH_DB, 'utf8');
+    console.log(contacts);
+    return JSON.parse(contacts);
+  } catch (error) {
+    throw error;
+  }
+};
