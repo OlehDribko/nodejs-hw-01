6d500e6a5bd311be5250cb1c90c276e42b7806d4
Ваше ім'я < email>
2025-05-04 21:29:14 +0300

    home work finish

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,6 +1,15 @@
+import { PATH_DB } from '../constants/contacts.js';
 import { readContacts } from '../utils/readContacts.js';
-
-export const removeLastContact = async () => {};
-const contacts = readContacts();
+import fs from 'fs/promises';
+export const removeLastContact = async () => {
+  try {
+    const contacts = await readContacts();
+    const newContactList = contacts.slice(0, contacts.length - 1);
+    fs.writeFile(PATH_DB, JSON.stringify(newContactList, null, 2));
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
 
 removeLastContact();
